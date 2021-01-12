0384de1ae5aea03277afa63db40181ac19bd9479
Kenneth Hedman <github@vitberget.se>
2021-01-12 22:38:22 +0100

    day 3 part 2

--- src/day3/day3part1.clj
+++ src/day3/day3part1.clj
@@ -22,7 +22,7 @@
            cnt 0]
       (let [r (nth forest y)
             c (nth r (mod x forest-width))
-            hit-tree (= c \# )
+            hit-tree (= c \#)
             hit-tree-cnt (+ cnt (if hit-tree 1 0))]
         (if (= (inc y) forest-height)
           hit-tree-cnt
@@ -32,9 +32,23 @@
             hit-tree-cnt))))))
 
 
+(defn day3part2
+  [forest list-steps]
+
+  (->> list-steps
+       (map (fn [s] (apply day3 forest s)))
+       (reduce *)
+       ))
+
 (comment
 
   (day3 "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
         3 1)
 
+  (day3part2 "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
+             [[1 1]
+              [3 1]
+              [5 1]
+              [7 1]
+              [1 2]])
   )
