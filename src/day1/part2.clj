8650a25f278776bd73f49890cd111925d2d2b87d
Kenneth Hedman <github@vitberget.se>
2021-01-12 14:07:56 +0100

    day1 part2 task description

---
+++ src/day1/part2.clj
@@ -0,0 +1,8 @@
+(ns day1.part2)
+;--- Part Two ---
+;
+;The Elves in accounting are thankful for your help; one of them even offers
+; you a starfish coin they had left over from a past vacation. They offer you a
+; second one if you can find three numbers in your expense report that meet the same criteria.
+;Using the above example again, the three entries that sum to 2020 are 979, 366, and 675.
+; Multiplying them together produces the answer, 241861950.
