125209219743133cd8ef50c14a43815f2d760e64
Kenneth Hedman <github@vitberget.se>
2021-01-14 18:11:57 +0100

    day13 part2

--- src/day13/day13part1.clj
+++ src/day13/day13part1.clj
@@ -8,14 +8,13 @@
            (is= (string->note (str "939\n"
                                    "7,13,x,x,59,x,31,19"))
                 {:earliest 939
-                 :busses   (list 7 13 0 0 59 0 31 19)})
-           )}
+                 :busses   (list 7 13 0 0 59 0 31 19)}))}
   [string]
   (let [[earliest busses] (str/split string #"\n")]
     {:earliest (read-string earliest)
      :busses   (->> busses
                     (re-seq #"[\dx]+")
-                    (map (fn [it] (if (= it "x") "0" it)))
+                    (map (fn [bus-id-or-x] (if (= bus-id-or-x "x") "0" bus-id-or-x)))
                     (map read-string))}))
 
 (defn minutes-past
@@ -39,11 +38,25 @@
   [string]
   (let [{earliest :earliest busses :busses} (string->note string)]
     (->> busses
-         (filter (fn [b] (not (= 0 b))))
-         (map (fn [bid] [bid (minutes-past earliest bid)]))
-         (sort (fn [[_ a] [_ b]] (< a b)))
+         (filter (fn [bus-id] (not (= 0 bus-id))))
+         (map (fn [bus-id] [bus-id (minutes-past earliest bus-id)]))
+         (sort (fn [[_ bus-id-a] [_ bus-id-b]] (< bus-id-a bus-id-b)))
          (first)
          (reduce *))))
+
+(defn bus-remainders
+  [timestamp bus-list]
+  (->> bus-list
+       (map (fn [[note-idx bus-id]]
+              [note-idx bus-id (mod (+ timestamp note-idx) bus-id)]))))
+
+(defn index-busses
+  [string]
+  (->> (string->note string)
+       (:busses)
+       (map-indexed (fn [note-id bus-id] [note-id bus-id]))
+       (filter (fn [[_ bus-id]] (> bus-id 0)))
+       (sort (fn [[_ bus-id-a] [_ bus-id-b]] (> bus-id-a bus-id-b)))))
 
 (defn day13part2
   {:test (fn []
@@ -54,10 +67,24 @@
            (is= (day13part2 (str "123\n" "67,7,x,59,61")) 1261476)
            (is= (day13part2 (str "123\n" "1789,37,47,1889")) 1202161486))}
   [string]
-  )
+  (let [indexed-busses (index-busses string)
+        [largest-bus-id-idx largest-bus-id] (first indexed-busses)]
+    (loop [timestamp (- largest-bus-id largest-bus-id-idx)]
+      (let [busses-with-remainder (bus-remainders timestamp indexed-busses)]
+        (if (= 0 (->> busses-with-remainder
+                      (map last)
+                      (reduce +)))
+          timestamp
+          (recur (+ timestamp
+                    (->> busses-with-remainder
+                         (filter (fn [[_ _ remainder]] (= remainder 0)))
+                         (map second)
+                         (reduce *)))))))))
 
 (comment
   (def my-note "1006726\n23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,647,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,557,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17"
     )
-  (day13part1 my-note)
+  (index-busses my-note)
+  (day13part1 my-note)                                      ; 3882
+  (day13part2 my-note)                                      ; 867295486378319
   )
