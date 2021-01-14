f8b2b37df4e4454c5420bab14c50c57dc167a3ab
Kenneth Hedman <github@vitberget.se>
2021-01-14 16:32:05 +0100

    defn day13part2 with failing test

--- src/day13/day13part1.clj
+++ src/day13/day13part1.clj
@@ -8,35 +8,53 @@
            (is= (string->note (str "939\n"
                                    "7,13,x,x,59,x,31,19"))
                 {:earliest 939
-                 :busses   (list 7 13 59 31 19)})
+                 :busses   (list 7 13 0 0 59 0 31 19)})
            )}
   [string]
   (let [[earliest busses] (str/split string #"\n")]
     {:earliest (read-string earliest)
      :busses   (->> busses
-                    (re-seq #"\d+")
+                    (re-seq #"[\dx]+")
+                    (map (fn [it] (if (= it "x") "0" it)))
                     (map read-string))}))
 
 (defn minutes-past
   {:test (fn []
-           (is= (minutes-past 939 59)
-                5))}
+           (is= (minutes-past 939 59) 5)
+           (is= (minutes-past 0 7) 0)
+           (is= (minutes-past 7 7) 0))}
   [earliest bus-id]
   (-> (/ earliest bus-id)
       (int)
       (inc)
       (* bus-id)
-      (- earliest)))
+      (- earliest)
+      (mod bus-id)))
 
 (defn day13part1
-  {:test (fn[]
+  {:test (fn []
            (is= (day13part1 (str "939\n"
-                                "7,13,x,x,59,x,31,19"))
+                                 "7,13,x,x,59,x,31,19"))
                 (* 5 59)))}
   [string]
   (let [{earliest :earliest busses :busses} (string->note string)]
     (->> busses
-         (map (fn[bid] [bid (minutes-past earliest bid)]))
-         (sort (fn[a b] (< (second a) (second b))))
+         (filter (fn [b] (not (= 0 b))))
+         (map (fn [bid] [bid (minutes-past earliest bid)]))
+         (sort (fn [[_ a] [_ b]] (< a b)))
          (first)
          (reduce *))))
+
+(defn day13part2
+  {:test (fn []
+           (is= (day13part2 (str "939\n"
+                                 "7,13,x,x,59,x,31,19"))
+                1068781))}
+  [string]
+  )
+
+(comment
+  (def my-note "1006726\n23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,647,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,557,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17"
+    )
+  (day13part1 my-note)
+  )
