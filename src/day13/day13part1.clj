cf84242f51ca302600e186c01a8e9f49d2bbc675
Kenneth Hedman <github@vitberget.se>
2021-01-16 15:12:41 +0100

    day13 a bit more presentable, for teaching Clojure and show how to solve this day

--- src/day13/day13part1.clj
+++ src/day13/day13part1.clj
@@ -1,7 +1,12 @@
 (ns day13.day13part1
-  (:require [ysera.test :refer [is is= is-not]]
+  (:require [ysera.test :refer [is is= is-not deftest]]
             [clojure.string :as str]))
 
+(defn x->0
+  [bus-id-or-x]
+  (if (= bus-id-or-x "x")
+    "0"
+    bus-id-or-x))
 
 (defn string->note
   {:test (fn []
@@ -10,11 +15,11 @@
                 {:earliest 939
                  :busses   (list 7 13 0 0 59 0 31 19)}))}
   [string]
-  (let [[earliest busses] (str/split string #"\n")]
+  (let [[earliest busses] (str/split-lines string)]
     {:earliest (read-string earliest)
      :busses   (->> busses
                     (re-seq #"[\dx]+")
-                    (map (fn [bus-id-or-x] (if (= bus-id-or-x "x") "0" bus-id-or-x)))
+                    (map x->0)
                     (map read-string))}))
 
 (defn minutes-past
@@ -30,6 +35,10 @@
       (- earliest)
       (mod bus-id)))
 
+(defn not-zero?
+  [number]
+  (not (zero? number)))
+
 (defn day13part1
   {:test (fn []
            (is= (day13part1 (str "939\n"
@@ -38,9 +47,10 @@
   [string]
   (let [{earliest :earliest busses :busses} (string->note string)]
     (->> busses
-         (filter (fn [bus-id] (not (= 0 bus-id))))
+         (filter not-zero?)
          (map (fn [bus-id] [bus-id (minutes-past earliest bus-id)]))
-         (sort (fn [[_ bus-id-a] [_ bus-id-b]] (< bus-id-a bus-id-b)))
+         (sort (fn [[_ bus-id-a] [_ bus-id-b]]
+                 (< bus-id-a bus-id-b)))
          (first)
          (reduce *))))
 
@@ -48,15 +58,18 @@
   [timestamp bus-list]
   (->> bus-list
        (map (fn [[note-idx bus-id]]
-              [note-idx bus-id (mod (+ timestamp note-idx) bus-id)]))))
+              [note-idx bus-id (-> (+ timestamp note-idx)
+                                   (mod bus-id))]))))
 
 (defn index-busses
   [string]
   (->> (string->note string)
        (:busses)
        (map-indexed (fn [note-id bus-id] [note-id bus-id]))
-       (filter (fn [[_ bus-id]] (> bus-id 0)))
-       (sort (fn [[_ bus-id-a] [_ bus-id-b]] (> bus-id-a bus-id-b)))))
+       (filter (fn [[_ bus-id]] (not-zero? bus-id)))
+       (sort (fn [[_ bus-id-a]
+                  [_ bus-id-b]]
+               (> bus-id-a bus-id-b)))))
 
 (defn day13part2
   {:test (fn []
@@ -71,20 +84,25 @@
         [largest-bus-id-idx largest-bus-id] (first indexed-busses)]
     (loop [timestamp (- largest-bus-id largest-bus-id-idx)]
       (let [busses-with-remainder (bus-remainders timestamp indexed-busses)]
-        (if (= 0 (->> busses-with-remainder
-                      (map last)
-                      (reduce +)))
+        (if (zero? (->> busses-with-remainder
+                        (map last)
+                        (reduce +)))
           timestamp
-          (recur (+ timestamp
-                    (->> busses-with-remainder
-                         (filter (fn [[_ _ remainder]] (= remainder 0)))
-                         (map second)
-                         (reduce *)))))))))
+          (recur (+ timestamp (->> busses-with-remainder
+                                   (filter (fn [[_ _ remainder]] (zero? remainder)))
+                                   (map second)
+                                   (reduce *)))))))))
+
+(deftest my-puzzle
+         (let [my-puzzle (str "1006726\n"
+                              "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,647,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,557,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17")]
+           (is= (day13part1 my-puzzle) 3882)
+           (is= (day13part2 my-puzzle) 867295486378319)))
+
 
 (comment
-  (def my-note "1006726\n23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,647,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,557,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17"
-    )
-  (index-busses my-note)
-  (day13part1 my-note)                                      ; 3882
-  (day13part2 my-note)                                      ; 867295486378319
+  (def my-puzzle (str "1006726\n"
+                      "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,647,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,557,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17"
+                      ""))
+  (index-busses my-puzzle)
   )
