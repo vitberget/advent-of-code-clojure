4eb5a71056fcb906e8afd6c8ab5904d44cfe01d3
Kenneth Hedman <github@vitberget.se>
2021-01-17 15:40:34 +0100

    day 15 part 2

--- src/day15/day15part1.clj
+++ src/day15/day15part1.clj
@@ -5,25 +5,44 @@
 (defn string->spoken-words
   {:test (fn []
            (is= (string->spoken-words "0,3,6")
-                (list 6 3 0)))}
+                {:word-map {0 1, 3 2}
+                 :number   6
+                 :idx      3})
+           (is= (string->spoken-words "0,3,6,0")
+                {:word-map {0 1, 3 2, 6 3}
+                 :number   0
+                 :idx      4})
+           (is= (string->spoken-words "0,3,6,0,4")
+                {:word-map {3 2, 6 3, 0 4}
+                 :number   4
+                 :idx      5}))}
   [string]
-  (->> (str/split string #",")
-       (map read-string)
-       (reverse)))
+  (let [numbers (->> (str/split string #",")
+                     (map read-string))]
+    {:number   (last numbers)
+     :word-map (->> numbers
+                    (drop-last)
+                    (map-indexed (fn [i n] {n (inc i)}))
+                    (reduce merge))
+     :idx      (count numbers)}))
 
+
+
+;0,3,6,0,3,3,0,4,0
 (defn last-spoken
   {:test (fn []
-           (is= (last-spoken (list 6 3 0)) 0)
-           (is= (last-spoken (list 0 6 3 0)) 3)
-           (is= (last-spoken (list 0 6 3 0 1 2 3)) 3))}
-
-  [[match & list]]
-  (loop [idx 1
-         [num & list] list]
-    (condp = num
-      nil 0
-      match idx
-      (recur (inc idx) list))))
+           (is= (last-spoken (string->spoken-words "0,3,6"))
+                (string->spoken-words "0,3,6,0"))
+           (is= (last-spoken (string->spoken-words "0,3,6,0"))
+                (string->spoken-words "0,3,6,0,3"))
+           (is= (last-spoken (string->spoken-words "0,3,6,0,3"))
+                (string->spoken-words "0,3,6,0,3,3")))}
+  [{word-map :word-map number :number idx :idx :as state}]
+  {:number   (if-let [number (get word-map number)]
+               (- idx number)
+               0)
+   :word-map (merge word-map {number idx})
+   :idx      (inc idx)})
 
 (defn nth-spoken
   {:test (fn []
@@ -32,14 +51,21 @@
            (is= (nth-spoken "1,2,3" 2020) 27)
            (is= (nth-spoken "2,3,1" 2020) 78)
            (is= (nth-spoken "3,2,1" 2020) 438)
-           (is= (nth-spoken "3,1,2" 2020) 1836))}
-  [string number]
-  (loop [spoken-words (string->spoken-words string)]
-    (if (= (count spoken-words) number)
-      (first spoken-words)
-      (recur (conj spoken-words (last-spoken spoken-words))))))
+           (is= (nth-spoken "3,1,2" 2020) 1836)
 
+           (comment (nth-spoken "1,3,2" 30000000) 2578)
+           (comment (nth-spoken "2,1,3" 30000000) 3544142)
+           (comment (nth-spoken "1,2,3" 30000000) 261214)
+           (comment (nth-spoken "2,3,1" 30000000) 6895259)
+           (comment (nth-spoken "3,2,1" 30000000) 18)
+           (comment (nth-spoken "3,1,2" 30000000) 362))}
+  [string wanted-index]
+  (loop [{idx :idx number :number :as state} (string->spoken-words string)]
+    (if (= wanted-index idx)
+      number
+      (recur (last-spoken state)))))
 
 (comment
-  (nth-spoken "14,8,16,0,1,17" 2020) ; 240
+  (nth-spoken "14,8,16,0,1,17" 2020)                        ; 240
+  (nth-spoken "14,8,16,0,1,17" 30000000)                    ; 505
   )
