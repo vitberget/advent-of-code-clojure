938f99259d8d02bb8e8cf8d52bb855c76786347d
Kenneth Hedman <github@vitberget.se>
2021-02-06 10:22:48 +0100

    day16 part1

--- src/year2020/day16/day16part1.clj
+++ src/year2020/day16/day16part1.clj
@@ -103,7 +103,7 @@
 
 (defn day16part1
   {:test (fn[]
-           (is= (day13part1 "class: 1-3 or 5-7\nrow: 6-11 or 33-44\nseat: 13-40 or 45-50\n\nyour ticket:\n7,1,14\n\nnearby tickets:\n7,3,47\n40,4,50\n55,2,20\n38,6,12")
+           (is= (day16part1 "class: 1-3 or 5-7\nrow: 6-11 or 33-44\nseat: 13-40 or 45-50\n\nyour ticket:\n7,1,14\n\nnearby tickets:\n7,3,47\n40,4,50\n55,2,20\n38,6,12")
                 71))}
   [string]
   (let [note (string->note string)
