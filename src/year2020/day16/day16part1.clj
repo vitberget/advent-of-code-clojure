cd23340f6b238d3bc4f768ab83d8bba68f5654e7
Kenneth Hedman <github@vitberget.se>
2021-02-06 10:23:26 +0100

    day16 part1

--- src/year2020/day16/day16part1.clj
+++ src/year2020/day16/day16part1.clj
@@ -102,7 +102,7 @@
                                  (map line->ticket))})))
 
 (defn day16part1
-  {:test (fn[]
+  {:test (fn []
            (is= (day16part1 "class: 1-3 or 5-7\nrow: 6-11 or 33-44\nseat: 13-40 or 45-50\n\nyour ticket:\n7,1,14\n\nnearby tickets:\n7,3,47\n40,4,50\n55,2,20\n38,6,12")
                 71))}
   [string]
@@ -110,7 +110,7 @@
         gibberish (:gibberish note)]
     (->> note
          (:nearby-tickets)
-         (map (fn[ticket] (get-invalid-numbers gibberish ticket)))
+         (map (fn [ticket] (get-invalid-numbers gibberish ticket)))
          (flatten)
          (filter identity)
          (reduce +))))
