e7848beacc6c188071971f78de89a540826b3b9d
Kenneth Hedman <github@vitberget.se>
2021-03-13 14:16:12 +0100

    Day 10, timing

--- src/year2020/day10/day10part2.clj
+++ src/year2020/day10/day10part2.clj
@@ -28,7 +28,7 @@
     (if (or (empty? numbers)
             (> (- jump number) 3))
       jump-set
-        (recur (conj jump-set jump) numbers))))
+      (recur (conj jump-set jump) numbers))))
 
 (defn possible-jumps
   [numbers]
@@ -62,8 +62,9 @@
        (count-possibilities-from 0)))
 
 (comment
-  (def puzzle "133\n157\n39\n74\n108\n136\n92\n55\n86\n46\n111\n58\n80\n115\n84\n67\n98\n30\n40\n61\n71\n114\n17\n9\n123\n142\n49\n158\n107\n139\n104\n132\n155\n96\n91\n15\n11\n23\n54\n6\n63\n126\n3\n10\n116\n87\n68\n72\n109\n62\n134\n103\n1\n16\n101\n117\n35\n120\n151\n102\n85\n145\n135\n79\n2\n147\n33\n41\n93\n52\n48\n64\n81\n29\n20\n110\n129\n43\n148\n36\n53\n26\n42\n156\n154\n77\n88\n73\n27\n34\n12\n146\n78\n47\n28\n97"
-    )
-  (day10part2 puzzle)
-  ; 6908379398144
+  (let [puzzle "133\n157\n39\n74\n108\n136\n92\n55\n86\n46\n111\n58\n80\n115\n84\n67\n98\n30\n40\n61\n71\n114\n17\n9\n123\n142\n49\n158\n107\n139\n104\n132\n155\n96\n91\n15\n11\n23\n54\n6\n63\n126\n3\n10\n116\n87\n68\n72\n109\n62\n134\n103\n1\n16\n101\n117\n35\n120\n151\n102\n85\n145\n135\n79\n2\n147\n33\n41\n93\n52\n48\n64\n81\n29\n20\n110\n129\n43\n148\n36\n53\n26\n42\n156\n154\n77\n88\n73\n27\n34\n12\n146\n78\n47\n28\n97"]
+    (time
+      (day10part2 puzzle)))
+  ; "Elapsed time: 0.70909 msecs"
+  ;=> 6908379398144
   )
