ba08ad0f2019ce7f93554b4686107eb19dde03e5
Kenneth Hedman <github@vitberget.se>
2021-04-28 09:28:40 +0200

    Day 24 part 1

---
+++ src/year2020/day24/day24example.clj
@@ -0,0 +1,24 @@
+(ns year2020.day24.day24example)
+
+(def example-part1
+  (str
+    "sesenwnenenewseeswwswswwnenewsewsw\n"
+    "neeenesenwnwwswnenewnwwsewnenwseswesw\n"
+    "seswneswswsenwwnwse\n"
+    "nwnwneseeswswnenewneswwnewseswneseene\n"
+    "swweswneswnenwsewnwneneseenw\ne"
+    "esenwseswswnenwswnwnwsewwnwsene\n"
+    "sewnenenenesenwsewnenwwwse\n"
+    "wenwwweseeeweswwwnwwe\n"
+    "wsweesenenewnwwnwsenewsenwwsesesenwne\n"
+    "neeswseenwwswnwswswnw\n"
+    "nenwswwsewswnenenewsenwsenwnesesenew\n"
+    "enewnwewneswsewnwswenweswnenwsenwsw\n"
+    "sweneswneswneneenwnewenewwneswswnese\n"
+    "swwesenesewenwneswnwwneseswwne\n"
+    "enesenwswwswneneswsenwnewswseenwsese\n"
+    "wnwnesenesenenwwnenwsewesewsesesew\n"
+    "nenewswnwewswnenesenwnesewesw\n"
+    "eneswnwswnwsenenwnwnwwseeswneewsenese\n"
+    "neswnwewnwnwseenwseesewsenwsweewe\n"
+    "wseweeenwnesenwwwswnew"))
