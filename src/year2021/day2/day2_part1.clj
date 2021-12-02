579c49bcfecfc021084e2452bea51e04a7c72765
Kenneth Hedman <github@vitberget.se>
2021-12-02 08:50:31 +0100

    2021 day 2 - nicer

--- src/year2021/day2/day2_part1.clj
+++ src/year2021/day2/day2_part1.clj
@@ -3,21 +3,28 @@
             [year2021.day2.data-day2 :refer [day2-example day2-puzzle]]
             [clojure.string :as str]))
 
+(defn text->actions
+  [text]
+  (->>
+    text
+    (str/split-lines)
+    (map (fn [line] (str/split line #" ")))
+    (map (fn [[word number]] [word (read-string number)]))))
+
+(defn update-state
+  [state [word number]]
+  (condp = word
+    "down" (update state :d + number)
+    "up" (update state :d - number)
+    "forward" (update state :f + number)))
+
 (defn day2-part1
   {:test (fn []
            (is= (day2-part1 day2-example) 150))}
   [text]
   (->> text
-       (str/split-lines)
-       (map (fn [line] (str/split line #" ")))
-       (map (fn [[word number]] [word (read-string number)]))
-       (reduce
-         (fn [state [word number]]
-           (condp = word
-             "down" (update state :d + number)
-             "up" (update state :d - number)
-             "forward" (update state :f + number)))
-         {:f 0 :d 0})
+       (text->actions)
+       (reduce update-state {:f 0 :d 0})
        (vals)
        (reduce *)))
 
