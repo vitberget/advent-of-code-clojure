b622cec3901cdc34b0ab05c3710e50196f5cea01
Kenneth Hedman <github@vitberget.se>
2021-12-02 17:10:18 +0100

    2021 day 3 placeholder

---
+++ src/year2021/day3/day3_data.clj
@@ -0,0 +1,5 @@
+(ns year2021.day3.day3-data)
+
+(def day3-example "")
+
+(def day3-puzzle "")
---
+++ src/year2021/day3/day3_part2.clj
@@ -0,0 +1,17 @@
+(ns year2021.day3.day3-part2
+  (:require [ysera.test :refer [is is= is-not deftest]]
+            [year2021.day3.day3-data :refer [day3-example day3-puzzle]]
+            [clojure.string :as str]))
+
+(defn day3-part2
+  {:test (fn []
+           (is= (day3-part2 day3-example) 0))}
+  [text]
+  )
+
+
+(comment
+  (time (day3-part2 day3-puzzle))
+  ;
+  ;
+  )
