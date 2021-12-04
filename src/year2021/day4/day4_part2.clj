d4059702c820fe11eec3f586bc605ce8ca81017b
Kenneth Hedman <github@vitberget.se>
2021-12-04 13:45:46 +0100

    2021 day 4 complete

--- src/year2021/day4/day4_part2.clj
+++ src/year2021/day4/day4_part2.clj
@@ -1,17 +1,29 @@
 (ns year2021.day4.day4-part2
   (:require [ysera.test :refer [is is= is-not deftest]]
             [year2021.day4.day4-data :refer [day4-example day4-puzzle]]
-            [clojure.string :as str]))
+            [year2021.day4.day4-part1 :refer [text->state
+                                              winning-bingoline-and-last-number
+                                              score-winner]]))
 
 (defn day4-part2
   {:test (fn []
-           (is= (day4-part2 day4-example) 0))}
+           (is= (day4-part2 day4-example) 1924))}
   [text]
-  )
-
+  (let [state (text->state text)
+        {numbers :numbers boards :boards} state]
+    (->> boards
+         (map (fn [board] {:numbers numbers
+                           :boards  (list board)}))
+         (map winning-bingoline-and-last-number)
+         (reduce (fn [w1 w2]
+                   (if (> (count (:numbers w1))
+                          (count (:numbers w2)))
+                     w1
+                     w2)))
+         (score-winner))))
 
 (comment
   (time (day4-part2 day4-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 206.578017 msecs"
+  ;=> 13158
   )
