56ebfe5e34709219da1981486db96ae574a36cfb
Kenneth Hedman <github@vitberget.se>
2021-12-05 10:14:55 +0100

    2021 day 5 complete

--- src/year2021/day5/day5_part1.clj
+++ src/year2021/day5/day5_part1.clj
@@ -3,15 +3,101 @@
             [year2021.day5.day5-data :refer [day5-example day5-puzzle]]
             [clojure.string :as str]))
 
+(defn line->line
+  [line]
+  (let [[p1 p2] (str/split line #" -> ")
+        [x1 y1] (str/split p1 #",")
+        [x2 y2] (str/split p2 #",")]
+    {:x1 (read-string x1)
+     :y1 (read-string y1)
+     :x2 (read-string x2)
+     :y2 (read-string y2)}))
+
+(defn text->state
+  [text]
+  (->> text
+       (str/split-lines)
+       (map line->line)))
+
+(defn get-direction
+  [n1 n2]
+  (cond
+    (= n1 n2) 0
+    (> n2 n1) 1
+    (> n1 n2) -1))
+
+(defn line->coverage
+  {:test (fn []
+           (is= (line->coverage {:x1 0, :y1 9, :x2 2, :y2 9})
+                #{{:x 0 :y 9}
+                  {:x 1 :y 9}
+                  {:x 2 :y 9}}))}
+  [{x1 :x1 y1 :y1 x2 :x2 y2 :y2}]
+  (let [dx (get-direction x1 x2)
+        dy (get-direction y1 y2)]
+    (loop [x x1
+           y y1
+           coverage #{}]
+      (let [coverage (conj coverage {:x x :y y})]
+        (if (and (= x x2)
+                 (= y y2))
+          coverage
+          (recur (+ x dx)
+                 (+ y dy)
+                 coverage))))))
+
+(defn coverage->map
+  {:test (fn []
+           (is= (coverage->map #{{:x 0 :y 9}
+                                 {:x 1 :y 9}
+                                 {:x 2 :y 9}})
+                {{:x 0 :y 9} 1
+                 {:x 1 :y 9} 1
+                 {:x 2 :y 9} 1}))}
+  [coverage]
+  (->> coverage
+       (reduce
+         (fn [the-map point]
+           (update the-map
+                   point
+                   (fn [n] (inc (or n 0)))))
+         {})))
+
+(defn update-the-map
+  {:test (fn []
+           (is= (update-the-map {{:x 0 :y 9} 1
+                                 {:x 1 :y 9} 1
+                                 {:x 2 :y 9} 1}
+                                {{:x 0 :y 9} 1
+                                 {:x 1 :y 9} 1})
+                {{:x 0 :y 9} 2
+                 {:x 1 :y 9} 2
+                 {:x 2 :y 9} 1}))}
+  [map1 map2]
+  (reduce
+    (fn [map1 [point _]]
+      (update map1 point
+              (fn[n] (inc (or n 0)))))
+    map1
+    map2))
+
 (defn day5-part1
   {:test (fn []
-           (is= (day5-part1 day5-example) 0))}
+           (is= (day5-part1 day5-example) 5))}
   [text]
-  )
-
+  (->> (text->state text)
+       (filter (fn [{x1 :x1 y1 :y1 x2 :x2 y2 :y2}]
+                 (or (= x1 x2)
+                     (= y1 y2))))
+       (map line->coverage)
+       (map coverage->map)
+       (reduce update-the-map)
+       (vals)
+       (filter (fn[n] (> n 1)))
+       (count)))
 
 (comment
   (time (day5-part1 day5-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 136.730561 msecs"
+  ;=> 6572
   )
--- src/year2021/day5/day5_part2.clj
+++ src/year2021/day5/day5_part2.clj
@@ -1,17 +1,25 @@
 (ns year2021.day5.day5-part2
   (:require [ysera.test :refer [is is= is-not deftest]]
             [year2021.day5.day5-data :refer [day5-example day5-puzzle]]
-            [clojure.string :as str]))
+            [year2021.day5.day5-part1 :refer [text->state
+                                              line->coverage
+                                              coverage->map
+                                              update-the-map]]))
 
 (defn day5-part2
   {:test (fn []
-           (is= (day5-part2 day5-example) 0))}
+           (is= (day5-part2 day5-example) 12))}
   [text]
-  )
-
+  (->> (text->state text)
+       (map line->coverage)
+       (map coverage->map)
+       (reduce update-the-map)
+       (vals)
+       (filter (fn[n] (> n 1)))
+       (count)))
 
 (comment
   (time (day5-part2 day5-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 249.182724 msecs"
+  ;=> 21466
   )
