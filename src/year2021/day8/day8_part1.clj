c28c9fb2a6c47cde965d19f335e2c8cee4916386
Kenneth Hedman <github@vitberget.se>
2021-12-08 11:39:37 +0100

    2021 day 8 part 1 complete

--- src/year2021/day8/day8_part1.clj
+++ src/year2021/day8/day8_part1.clj
@@ -5,13 +5,23 @@
 
 (defn day8-part1
   {:test (fn []
-           (is= (day8-part1 day8-example) 0))}
+           (is= (day8-part1 day8-example) 26))}
   [text]
-  )
-
+  (as-> text $
+        (str/split-lines $)
+        (map (fn [line] (second (str/split line #" \| "))) $)
+        (map str/trim $)
+        (mapcat (fn [line] (str/split line #"\s+")) $)
+        (filter (fn [word] (let [c (count word)]
+                             (or (= c 2) ; 1
+                                 (= c 4) ; 4
+                                 (= c 3) ; 7
+                                 (= c 7)))) ; 8
+                $)
+        (count $)))
 
 (comment
   (time (day8-part1 day8-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 7.468041 msecs"
+  ;=> 387
   )
