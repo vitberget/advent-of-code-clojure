60157b54e71cbbd97c58d526bddb81de410d2bb7
Kenneth Hedman <github@vitberget.se>
2021-12-12 23:07:08 +0100

    2021 day 12 part 1 complete

--- src/year2021/day12/day12_data.clj
+++ src/year2021/day12/day12_data.clj
@@ -1,5 +1,69 @@
 (ns year2021.day12.day12-data)
 
-(def day12-example "")
+(def day12-example-1 "start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end")
+;start-A
+;start-b
+;A-c
+;A-b
+;b-d
+;A-end
+;b-end
 
-(def day12-puzzle "")
+(def day12-example-2 "dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc")
+;dc-end
+;HN-start
+;start-kj
+;dc-start
+;dc-HN
+;LN-dc
+;HN-end
+;kj-sa
+;kj-HN
+;kj-dc
+
+(def day12-example-3 "fs-end\nhe-DX\nfs-he\nstart-DX\npj-DX\nend-zg\nzg-sl\nzg-pj\npj-he\nRW-he\nfs-DX\npj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW")
+;fs-end
+;he-DX
+;fs-he
+;start-DX
+;pj-DX
+;end-zg
+;zg-sl
+;zg-pj
+;pj-he
+;RW-he
+;fs-DX
+;pj-RW
+;zg-RW
+;start-pj
+;he-WI
+;zg-he
+;pj-fs
+;start-RW
+
+(def day12-puzzle "fw-ll\nend-dy\ntx-fw\ntx-tr\ndy-jb\nZD-dy\ndy-BL\ndy-tr\ndy-KX\nKX-start\nKX-tx\nfw-ZD\ntr-end\nfw-jb\nfw-yi\nZD-nr\nstart-fw\ntx-ll\nll-jb\nyi-jb\nyi-ll\nyi-start\nZD-end\nZD-jb\ntx-ZD")
+;fw-ll
+;end-dy
+;tx-fw
+;tx-tr
+;dy-jb
+;ZD-dy
+;dy-BL
+;dy-tr
+;dy-KX
+;KX-start
+;KX-tx
+;fw-ZD
+;tr-end
+;fw-jb
+;fw-yi
+;ZD-nr
+;start-fw
+;tx-ll
+;ll-jb
+;yi-jb
+;yi-ll
+;yi-start
+;ZD-end
+;ZD-jb
+;tx-ZD
--- src/year2021/day12/day12_part1.clj
+++ src/year2021/day12/day12_part1.clj
@@ -1,17 +1,113 @@
 (ns year2021.day12.day12-part1
   (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2021.day12.day12-data :refer [day12-example day12-puzzle]]
-            [clojure.string :as str]))
+            [ysera.collections :refer [seq-contains?]]
+            [year2021.day12.day12-data :refer [day12-example-1
+                                               day12-example-2
+                                               day12-example-3
+                                               day12-puzzle]]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn inner-update-cave-system
+  [cave-system key-str value-str]
+  (if (or (= key-str "end")
+          (= value-str "start"))
+    cave-system
+    (update cave-system key-str
+            (fn [value-set]
+              (if value-set
+                (conj value-set value-str)
+                #{value-str})))))
+
+(defn pair->cave-system
+  [cave-system [v1 v2]]
+  (-> cave-system
+      (inner-update-cave-system v1 v2)
+      (inner-update-cave-system v2 v1)))
+
+(defn text->cave-system
+  {:test (fn []
+           (is= (text->cave-system day12-example-1)
+                {"start" #{"A" "b"}
+                 "c"     #{"A"}
+                 "A"     #{"end" "b" "c"}
+                 "b"     #{"A" "d" "end"}
+                 "d"     #{"b"}}))}
+  [text]
+  (->> text
+       (str/split-lines)
+       (map #(str/split % #"-"))
+       (reduce pair->cave-system
+               {})))
+
+(defn lower-case?
+  [text]
+  (every? (fn [^Character x] (Character/isLowerCase x)) text))
+
+(defn path-contains-lowercase-cave?
+  {:test (fn []
+           (is (path-contains-lowercase-cave? ["start" "A" "b"] "b"))
+           (is (path-contains-lowercase-cave? ["start" "A" "c" "b"] "b"))
+           (is-not (path-contains-lowercase-cave? ["start" "A" "B"] "z"))
+           (is-not (path-contains-lowercase-cave? ["start" "A" "B"] "A")))}
+  [path cave]
+  (and (lower-case? cave)
+       (seq-contains? path cave)))
+
+(defn cave-system->paths
+  {:test (fn []
+           (is= (cave-system->paths (text->cave-system day12-example-1))
+                #{["start" "A" "b" "A" "c" "A" "end"]
+                  ["start" "A" "b" "A" "end"]
+                  ["start" "A" "b" "end"]
+                  ["start" "A" "c" "A" "b" "A" "end"]
+                  ["start" "A" "c" "A" "b" "end"]
+                  ["start" "A" "c" "A" "end"]
+                  ["start" "A" "end"]
+                  ["start" "b" "A" "c" "A" "end"]
+                  ["start" "b" "A" "end"]
+                  ["start" "b" "end"]}))}
+  ([cave-system]
+   (cave-system->paths cave-system ["start"]))
+  ([cave-system path]
+   (loop [paths #{}
+          path path
+          [next-cave & rest-of-caves] (get cave-system (last path))]
+     (cond
+       (nil? next-cave)
+       paths
+
+       (= next-cave "end")
+       (recur (conj paths (conj path "end"))
+              path
+              rest-of-caves)
+
+       (path-contains-lowercase-cave? path next-cave)
+       (recur paths
+              path
+              rest-of-caves)
+
+       :else
+       (recur (set/union paths (cave-system->paths cave-system (conj path next-cave)))
+              path
+              rest-of-caves)))))
+
 
 (defn day12-part1
   {:test (fn []
-           (is= (day12-part1 day12-example) 0))}
+           (is= (day12-part1 day12-example-1) 10)
+           (is= (day12-part1 day12-example-2) 19)
+           (is= (day12-part1 day12-example-3) 226)
+           (is= (day12-part1 day12-puzzle) 4573))}
   [text]
-  )
+  (-> text
+      (text->cave-system)
+      (cave-system->paths)
+      (count)))
 
 
 (comment
   (time (day12-part1 day12-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 35.06007 msecs"
+  ;=> 4573
   )
