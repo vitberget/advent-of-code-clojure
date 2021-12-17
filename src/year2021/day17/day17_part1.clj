1b44b4b826fcc69b10bba1345b1cecdc85317d50
Kenneth Hedman <github@vitberget.se>
2021-12-17 08:16:54 +0100

    2021 day 17 part 1 complete

--- src/year2021/day17/day17_part1.clj
+++ src/year2021/day17/day17_part1.clj
@@ -1,17 +1,71 @@
 (ns year2021.day17.day17-part1
   (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2021.day17.day17-data :refer [day17-example day17-puzzle]]
-            [clojure.string :as str]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.math.numeric-tower :as math]))
+
+(def day17-example "target area: x=20..30, y=-10..-5")
+(def day17-puzzle "target area: x=156..202, y=-110..-69")
+
+(defn text->target
+  {:test (fn []
+           (is= (text->target day17-example)
+                {:x-min 20
+                 :x-max 30
+                 :y-min -10
+                 :y-max -5}))}
+  [text]
+  (let [numbers (->> text
+                     (re-matches #"target area: x=([0-9\-]+)..([0-9\-]+), y=([0-9\-]+)..([0-9\-]+)")
+                     (drop 1)
+                     (map read-string))]
+    {:x-min (apply min (take 2 numbers))
+     :x-max (apply max (take 2 numbers))
+     :y-min (apply min (drop 2 numbers))
+     :y-max (apply max (drop 2 numbers))}))
+
+(defn inside-range?
+  [n n-min n-max]
+  (and (>= n n-min)
+       (<= n n-max)))
+
+(defn y-speed-ok?
+  [y-speed y-min y-max]
+  (loop [y-pos 0
+         y-speed y-speed]
+    (cond
+      (< y-pos y-min)
+      false
+
+      (inside-range? y-pos y-min y-max)
+      true
+
+      :else
+      (recur (+ y-pos y-speed)
+             (dec y-speed)))))
+
+(defn find-y-speed
+  [{y-min :y-min y-max :y-max}]
+  (loop [y-speed (math/abs y-min)]
+    (if (y-speed-ok? y-speed y-min y-max)
+      y-speed
+      (recur (dec y-speed)))))
+
+(defn find-max-y
+  [y-speed]
+  (reduce + y-speed (range (max y-speed))))
 
 (defn day17-part1
   {:test (fn []
-           (is= (day17-part1 day17-example) 0))}
+           (is= (day17-part1 day17-example) 45)
+           (is= (day17-part1 day17-puzzle) 5995))}
   [text]
-  )
+  (let [target (text->target text)
+        y-speed (find-y-speed target)]
+    (find-max-y y-speed)))
 
 
 (comment
   (time (day17-part1 day17-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 1.137415 msecs"
+  ;=> 5995
   )
