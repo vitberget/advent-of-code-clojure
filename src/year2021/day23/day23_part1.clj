1025f63c37de9f82947fad89cca9e2e3a2c9dbd9
Kenneth Hedman <github@vitberget.se>
2022-07-15 01:17:16 +0200

    2021 day 23 - text->state

--- src/year2021/day23/day23_part1.clj
+++ src/year2021/day23/day23_part1.clj
@@ -3,6 +3,35 @@
             [year2021.day23.day23-data :refer [day23-example day23-puzzle]]
             [clojure.string :as str]))
 
+(defn text->state
+  {:test (fn []
+           (is= (text->state day23-example)
+                {:A #{[3 2] [9 2]}
+                 :B #{[3 1] [7 1]}
+                 :C #{[5 1] [7 2]}
+                 :D #{[5 2] [9 1]}}))
+   }
+  [text]
+  (->> text
+       (str/split-lines)
+       (drop 2)
+       (take 2)
+       (map (fn [line]
+              (->> line
+                   (filter (fn [c] (contains? #{\A \B \C \D} c))))))
+       (map-indexed (fn [y line] (let [y (inc y)]
+                                   (->> line
+                                        (map-indexed (fn [x c]
+                                                       (let [x (-> x (* 2) (+ 3))]
+                                                         [(keyword (str c)) x y])))))))
+       (apply concat)
+       (reduce (fn [acc [c x y]]
+                 (update acc c (fn [e]
+                                   (if (nil? e)
+                                     #{[x y]}
+                                     (conj e [x y])))))
+               {})))
+
 (defn day23-part1
   {:test (fn []
            (is= (day23-part1 day23-example) 0))}
