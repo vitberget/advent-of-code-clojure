b2af037a9c66a6770d924df68790100e44e3068e
Kenneth Hedman <github@vitberget.se>
2022-12-01 10:49:12 +0100

    2022 day 1 part 1

--- src/year2022/day1/day1_part1.clj
+++ src/year2022/day1/day1_part1.clj
@@ -3,15 +3,29 @@
             [year2022.day1.day1-data :refer [day1-example day1-puzzle]]
             [clojure.string :as str]))
 
+
+(defn text->numbers
+  [text]
+  (->> text
+       (str/split-lines)
+       (map read-string)))
+
+(defn sum-numbers
+  [numbers]
+  (apply + numbers))
+
 (defn day1-part1
   {:test (fn []
-           (is= (day1-part1 day1-example) 0))}
+           (is= (day1-part1 day1-example) 24000))}
   [text]
-  )
-
+  (as-> text $
+        (str/split $ #"\n\n")
+        (map text->numbers $)
+        (map sum-numbers $)
+        (apply max $)))
 
 (comment
   (time (day1-part1 day1-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 13.852631 msecs"
+  ;=> 73211
   )
