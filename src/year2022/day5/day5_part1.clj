b40bbd8321e3646a839606b8553937c70b7fa375
Kenneth Hedman <github@vitberget.se>
2022-12-05 11:32:48 +0100

    2022 day 5 part 1 complete

--- src/year2022/day5/day5_part1.clj
+++ src/year2022/day5/day5_part1.clj
@@ -19,11 +19,11 @@
                           (last)
                           (read-string))
         lines (drop-last lines)]
-    (for [i (range no-of-stacks)]
-      (let [x (inc (* i 4))]
-        (->> lines
-             (map #(nth % x))
-             (filter #(not (= % \space))))))))
+    (into [] (for [i (range no-of-stacks)]
+               (let [x (inc (* i 4))]
+                 (->> lines
+                      (map #(nth % x))
+                      (filter #(not (= % \space)))))))))
 
 (defn text->moves
   {:test (fn []
@@ -31,12 +31,17 @@
                 [[1 2 1]
                  [3 1 3]
                  [2 2 1]
-                 [1 1 2]]))}
+                 [1 1 2]])
+           (is= (text->moves "move 3 from 2 to 9\nmove 1 from 1 to 6\nmove 6 from 6 to 7\nmove 13 from 7 to 6")
+                [[3 2 9]
+                 [1 1 6]
+                 [6 6 7]
+                 [13 7 6]]))}
   [text]
   (->> text
        (ut/text->lines)
-       (map #(re-matches #".*([0-9]+).*([0-9]+).*([0-9]+).*" %))
-       (map #(drop 1 %))
+       (map #(str/split % #"\s+"))
+       (map (fn [[_ n1 _ n2 _ n3]] [n1 n2 n3]))
        (map #(map read-string %))))
 
 (defn text->state
@@ -54,14 +59,60 @@
     {:stacks (text->stacks stack-text)
      :moves  (text->moves moves-text)}))
 
+(defn move-stacks
+  {:test (fn []
+           (let [step-1 [(list \N \Z)
+                         (list \D \C \M)
+                         (list \P)]
+                 step-2 [(list \D \N \Z)
+                         (list \C \M)
+                         (list \P)]
+                 step-3 [(list)
+                         (list \C \M)
+                         (list \Z \N \D \P)]]
+             (is= (move-stacks step-1 [1 2 1]) step-2)
+             (is= (move-stacks step-2 [3 1 3]) step-3)))}
+  [stacks [size from to]]
+  (let [from (dec from)
+        to (dec to)]
+    (loop [stacks stacks
+           size size]
+      (if (zero? size)
+        stacks
+        (recur
+          (-> stacks
+              (update to #(conj % (first (nth stacks from))))
+              (update from #(drop 1 %)))
+          (dec size))))))
+
+(defn text->state
+  {:test (fn []
+           (is= (text->state day5-example)
+                {:stacks [(list \N \Z)
+                          (list \D \C \M)
+                          (list \P)]
+                 :moves  [[1 2 1]
+                          [3 1 3]
+                          [2 2 1]
+                          [1 1 2]]}))}
+  [text]
+  (let [[stack-text moves-text] (ut/text->paragraphs text)]
+    {:stacks (text->stacks stack-text)
+     :moves  (text->moves moves-text)}))
 
 (defn day5-part1
   {:test (fn []
-           (is= (day5-part1 day5-example) "CMZ"))}
+           (is= (day5-part1 day5-example) "CMZ")
+           (is= (day5-part1 day5-puzzle) "TLFGBZHCN"))}
   [text]
-  )
+  (let [{stacks :stacks moves :moves} (text->state text)]
+    (->> moves
+         (reduce move-stacks stacks)
+         (map first)
+         (apply str))))
 
 (comment
   (time (day5-part1 day5-puzzle))
-  ;
+  ;"Elapsed time: 23.613165 msecs"
+  ;=> "TLFGBZHCN"
   )
