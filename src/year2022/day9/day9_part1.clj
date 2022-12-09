7664d4962d092881d4c6caaab4847549a53d0bc0
Kenneth Hedman <github@vitberget.se>
2022-12-09 11:04:53 +0100

    2022 day 9 part 1 complete

--- src/year2022/day9/day9_part1.clj
+++ src/year2022/day9/day9_part1.clj
@@ -1,17 +1,93 @@
 (ns year2022.day9.day9-part1
-  (:require [ysera.test :refer [is is= is-not deftest]]
+  (:require [utils.text :as ut]
             [year2022.day9.day9-data :refer [day9-example day9-puzzle]]
-            [clojure.string :as str]))
+            [ysera.test :refer [is is-not is=]]))
+
+(defn line->move
+  [line]
+  (let [[direction speed] (ut/line->words line)]
+    [(keyword direction) (read-string speed)]))
+
+(defn text->moves
+  [text]
+  (->> text
+       (ut/text->lines)
+       (map line->move)))
+
+(defn tail-touching-head?
+  {:test (fn []
+           (is (tail-touching-head? [0 0] [0 0]))
+           (is (tail-touching-head? [0 0] [1 0]))
+           (is (tail-touching-head? [5 5] [5 5]))
+           (is (tail-touching-head? [0 0] [1 1]))
+           (is (tail-touching-head? [2 2] [1 1]))
+           (is-not (tail-touching-head? [2 0] [0 0]))
+           (is-not (tail-touching-head? [2 0] [0 1])))}
+  [head tail]
+  (->> (map - head tail)
+       (map abs)
+       (map #(< % 2))
+       (reduce #(and %1 %2))))
+
+(defn tail-follow-head
+  {:test (fn []
+           (is= (tail-follow-head [0 0] [0 0]) [0 0])
+           (is= (tail-follow-head [0 0] [0 2]) [0 1])
+           (is= (tail-follow-head [0 0] [0 -2]) [0 -1])
+           (is= (tail-follow-head [0 0] [2 0]) [1 0])
+           (is= (tail-follow-head [0 0] [-2 0]) [-1 0])
+           (is= (tail-follow-head [1 2] [0 0]) [1 1])
+           (is= (tail-follow-head [2 1] [0 0]) [1 1]))}
+  [head [tx ty :as tail]]
+  (if (tail-touching-head? head tail)
+    tail
+    (let [[dx dy] (map - head tail)]
+      (cond
+        (zero? dx)
+        [tx (+ ty (if (pos? dy) 1 -1))]
+
+        (zero? dy)
+        [(+ tx (if (pos? dx) 1 -1)) ty]
+
+        (= 2 (abs dx))
+        [(+ tx (if (pos? dx) 1 -1)) (+ ty dy)]
+
+        (= 2 (abs dy))
+        [(+ tx dx) (+ ty (if (pos? dy) 1 -1))]))))
+
+(def directions
+  {:R [0 1]
+   :L [0 -1]
+   :U [1 0]
+   :D [-1 0]})
+
+(defn rope-a-dope
+  [state [direction speed]]
+  (loop [{head :head tail :tail :as state} state
+         speed speed]
+    (if (zero? speed)
+      state
+      (let [head (mapv + head (get directions direction))
+            tail (tail-follow-head head tail)]
+        (recur (-> state
+                   (update :tail-visitations conj tail)
+                   (assoc :head head :tail tail))
+               (dec speed))))))
 
 (defn day9-part1
   {:test (fn []
-           (is= (day9-part1 day9-example) 0))}
+           (is= (day9-part1 day9-example) 13)
+           (is= (day9-part1 day9-puzzle) 6745))}
   [text]
-  )
+  (->> text
+       (text->moves)
+       (reduce rope-a-dope {:head [0 0] :tail [0 0] :tail-visitations #{[0 0]}})
+       (:tail-visitations)
+       (count)))
 
 
 (comment
   (time (day9-part1 day9-puzzle))
-  ;
-  ;
+  ;"Elapsed time: 48.04134 msecs"
+  ;=> 6745
   )
