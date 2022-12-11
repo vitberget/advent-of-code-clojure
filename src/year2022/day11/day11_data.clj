5de2fceaec76b7e90572063b9fa117aaf53bc56b
Kenneth Hedman <github@vitberget.se>
2022-12-11 11:53:41 +0100

    2022 day 11 - progress

--- src/year2022/day11/day11_data.clj
+++ src/year2022/day11/day11_data.clj
@@ -1,5 +1,87 @@
 (ns year2022.day11.day11-data)
 
-(def day11-example "")
+(def day11-example "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1")
+;Monkey 0:
+;  Starting items: 79, 98
+;  Operation: new = old * 19
+;  Test: divisible by 23
+;    If true: throw to monkey 2
+;    If false: throw to monkey 3
+;
+;Monkey 1:
+;  Starting items: 54, 65, 75, 74
+;  Operation: new = old + 6
+;  Test: divisible by 19
+;    If true: throw to monkey 2
+;    If false: throw to monkey 0
+;
+;Monkey 2:
+;  Starting items: 79, 60, 97
+;  Operation: new = old * old
+;  Test: divisible by 13
+;    If true: throw to monkey 1
+;    If false: throw to monkey 3
+;
+;Monkey 3:
+;  Starting items: 74
+;  Operation: new = old + 3
+;  Test: divisible by 17
+;    If true: throw to monkey 0
+;    If false: throw to monkey 1
 
-(def day11-puzzle "")
+(def day11-puzzle "Monkey 0:\n  Starting items: 72, 97\n  Operation: new = old * 13\n  Test: divisible by 19\n    If true: throw to monkey 5\n    If false: throw to monkey 6\n\nMonkey 1:\n  Starting items: 55, 70, 90, 74, 95\n  Operation: new = old * old\n  Test: divisible by 7\n    If true: throw to monkey 5\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 74, 97, 66, 57\n  Operation: new = old + 6\n  Test: divisible by 17\n    If true: throw to monkey 1\n    If false: throw to monkey 0\n\nMonkey 3:\n  Starting items: 86, 54, 53\n  Operation: new = old + 2\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 2\n\nMonkey 4:\n  Starting items: 50, 65, 78, 50, 62, 99\n  Operation: new = old + 3\n  Test: divisible by 11\n    If true: throw to monkey 3\n    If false: throw to monkey 7\n\nMonkey 5:\n  Starting items: 90\n  Operation: new = old + 4\n  Test: divisible by 2\n    If true: throw to monkey 4\n    If false: throw to monkey 6\n\nMonkey 6:\n  Starting items: 88, 92, 63, 94, 96, 82, 53, 53\n  Operation: new = old + 8\n  Test: divisible by 5\n    If true: throw to monkey 4\n    If false: throw to monkey 7\n\nMonkey 7:\n  Starting items: 70, 60, 71, 69, 77, 70, 98\n  Operation: new = old * 7\n  Test: divisible by 3\n    If true: throw to monkey 2\n    If false: throw to monkey 3")
+;Monkey 0:
+;  Starting items: 72, 97
+;  Operation: new = old * 13
+;  Test: divisible by 19
+;    If true: throw to monkey 5
+;    If false: throw to monkey 6
+;
+;Monkey 1:
+;  Starting items: 55, 70, 90, 74, 95
+;  Operation: new = old * old
+;  Test: divisible by 7
+;    If true: throw to monkey 5
+;    If false: throw to monkey 0
+;
+;Monkey 2:
+;  Starting items: 74, 97, 66, 57
+;  Operation: new = old + 6
+;  Test: divisible by 17
+;    If true: throw to monkey 1
+;    If false: throw to monkey 0
+;
+;Monkey 3:
+;  Starting items: 86, 54, 53
+;  Operation: new = old + 2
+;  Test: divisible by 13
+;    If true: throw to monkey 1
+;    If false: throw to monkey 2
+;
+;Monkey 4:
+;  Starting items: 50, 65, 78, 50, 62, 99
+;  Operation: new = old + 3
+;  Test: divisible by 11
+;    If true: throw to monkey 3
+;    If false: throw to monkey 7
+;
+;Monkey 5:
+;  Starting items: 90
+;  Operation: new = old + 4
+;  Test: divisible by 2
+;    If true: throw to monkey 4
+;    If false: throw to monkey 6
+;
+;Monkey 6:
+;  Starting items: 88, 92, 63, 94, 96, 82, 53, 53
+;  Operation: new = old + 8
+;  Test: divisible by 5
+;    If true: throw to monkey 4
+;    If false: throw to monkey 7
+;
+;Monkey 7:
+;  Starting items: 70, 60, 71, 69, 77, 70, 98
+;  Operation: new = old * 7
+;  Test: divisible by 3
+;    If true: throw to monkey 2
+;    If false: throw to monkey 3
--- src/year2022/day11/day11_part1.clj
+++ src/year2022/day11/day11_part1.clj
@@ -1,14 +1,149 @@
 (ns year2022.day11.day11-part1
-  (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2022.day11.day11-data :refer [day11-example day11-puzzle]]
-            [clojure.string :as str]))
+  (:require
+    [utils.text :as ut]
+    [ysera.test :refer [is is= is-not deftest]]
+    [year2022.day11.day11-data :refer [day11-example day11-puzzle]]
+    [clojure.string :as str]))
+
+(defn words->operation
+  [[v1 v2 v3]]
+  [(if (= v1 "old") v1 (read-string v1))
+   v2
+   (if (= v3 "old") v3 (read-string v3))])
+
+(defn last-word->number
+  [words]
+  (->> words
+       (ut/line->words)
+       (last)
+       (read-string)))
+
+(defn paragraph->state
+  {:test (fn []
+           (is= (paragraph->state "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3")
+                ;Monkey 0:
+                ;  Starting items: 79, 98
+                ;  Operation: new = old * 19
+                ;  Test: divisible by 23
+                ;    If true: throw to monkey 2
+                ;    If false: throw to monkey 3
+                {"0" {:items     [79 98]
+                      :operation ["old" "*" 19]
+                      :test      23
+                      :if-true   "2"
+                      :if-false  "3"
+                      :inspected 0}}))}
+  [paragraph]
+  (let [lines (ut/text->lines paragraph)
+        monkey-number (-> lines
+                          (first)
+                          (ut/line->words)
+                          (second)
+                          (str/replace ":" ""))
+        items (->> lines
+                   (second)
+                   (ut/line->words)
+                   (drop 4)
+                   (map #(str/replace % "," ""))
+                   (map read-string))
+        operation (->> (nth lines 2)
+                       (ut/line->words)
+                       (drop 5)
+                       (words->operation))
+        test (->> (nth lines 3)
+                  (last-word->number))
+        if-true (->> (nth lines 4)
+                     (ut/line->words)
+                     (last))
+        if-false (->> (nth lines 5)
+                      (ut/line->words)
+                      (last))]
+    {monkey-number {:items     items
+                    :operation operation
+                    :test      test
+                    :if-true   if-true
+                    :if-false  if-false
+                    :inspected 0}}))
+(defn text->state
+  [text]
+  (->> text
+       (ut/text->paragraphs)
+       (map paragraph->state)
+       (apply merge)))
+
+
+(defn apply-operation
+  [number [v1 v2 v3]]
+  (let [v2 (condp = v2
+             "*" *
+             "/" /
+             "-" -
+             "+" +)]
+    (-> (v2 (if (= v1 "old") number v1)
+            (if (= v3 "old") number v3))
+        (quot 3))))
+
+
+(defn monkey-do
+  [state monkey-id]
+  (println)
+  (println "monkey-do" monkey-id)
+  (println ">" (get state "0"))
+  (println ">" (get state "1"))
+  (println ">" (get state "2"))
+  (println ">" (get state "3"))
+  (let [{operation :operation
+         test      :test
+         if-true   :if-true
+         if-false  :if-false} (get state monkey-id)
+        _ (println "if true" if-true "if false" if-false)
+        action-list-1 (->> (get-in state [monkey-id :items])
+                           (map #(apply-operation % operation))
+                           (map (fn [number] [number (mod number test)])))
+        action-list (->> action-list-1
+                         (map (fn [[number ihms]] [number (if (zero? ihms) if-true if-false)])))
+        _ (println "a1" action-list-1)
+        _ (println "al" action-list)
+        state (->> action-list
+                   (reduce (fn [state [number receiver]]
+                             (update-in state [receiver :items] conj number))
+                           state)
+                   )
+        state (-> state
+                  (assoc-in [monkey-id :items] (list))
+                  (update-in [monkey-id :inspected] + (count action-list)))]
+    (println "<" (get state "0"))
+    (println "<" (get state "1"))
+    (println "<" (get state "2"))
+    (println "<" (get state "3"))
+    state
+    ))
+
+(defn monkees-do
+  [state]
+  (let [monkees (->> state
+                     (keys)
+                     (sort))]
+    (reduce monkey-do
+            state
+            monkees)))
+
+(defn monkees-do-throw-stuff
+  [times state]
+  (reduce (fn [state _] (monkees-do state))
+          state
+          (range times)))
 
 (defn day11-part1
   {:test (fn []
-           (is= (day11-part1 day11-example) 0))}
+           (is= (day11-part1 day11-example) 10605))}
   [text]
-  )
-
+  (->> text
+       (text->state)
+       (monkees-do-throw-stuff 1)
+       (vals)
+       (map :items)
+       ))
 
 (comment
   (time (day11-part1 day11-puzzle))
