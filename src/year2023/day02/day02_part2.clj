e5c82c04f8cfd628dd3e60843a0795f77cf652bf
Kenneth Hedman <github@vitberget.se>
2023-12-02 11:14:51 +0100

    year2023 day02 complete

--- src/year2023/day02/day02_part2.clj
+++ src/year2023/day02/day02_part2.clj
@@ -1,17 +1,43 @@
 (ns year2023.day02.day02-part2
-  (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2023.day02.day02-part1 :as part-1]
-            [year2023.day02.day02-data :refer [day02-example day02-puzzle]]
-            [clojure.string :as str]))
+  (:require
+    [utils.text :refer [text->lines]]
+    [year2023.day02.day02-data :refer [day02-example day02-puzzle]]
+    [ysera.test :refer [is=]]
+    [year2023.day02.day02-part1 :as part-1]))
+
+(defn biggest-pick 
+  [{red-1 :red blue-1 :blue green-1 :green}
+   {red-2 :red blue-2 :blue green-2 :green}]
+  {:red (max (or red-1 0) (or red-2 0))
+   :blue (max (or blue-1 0) (or blue-2 0))
+   :green (max (or green-1 0) (or green-2 0))})
+
+(defn game->biggest-pick 
+  {:test (fn[] (is= (game->biggest-pick (part-1/line->game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
+                    {:red 4 :green 2 :blue 6}))}
+  [game]
+  (->> game
+       (:picks)       
+       (reduce biggest-pick)))
+
+(defn- power-pick [pick]
+  (->> pick
+       (vals)
+       (apply *)))
 
 (defn day02-part2
   {:test (fn []
-           (is= (day02-part2 day02-example) 0))}
+           (is= (day02-part2 day02-example) 2286))} 
   [text]
-  )
-
+  (->> text
+       (text->lines)
+       (map part-1/line->game)
+       (map game->biggest-pick)
+       (map power-pick)
+       (apply +)))
 
 (comment
   (time (day02-part2 day02-puzzle))
-  ;
+  ; (out) "Elapsed time: 5.772383 msecs"
+  ; 71036
   )
