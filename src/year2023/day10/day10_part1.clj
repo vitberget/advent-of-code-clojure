13d927fcf2a870c98f7ee9908fb5f0c8514b5607
Kenneth Hedman <github@vitberget.se>
2023-12-10 11:58:54 +0100

    year2023 day10

--- src/year2023/day10/day10_part1.clj
+++ src/year2023/day10/day10_part1.clj
@@ -1,16 +1,87 @@
 (ns year2023.day10.day10-part1
-  (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2023.day10.day10-data :refer [day10-example day10-puzzle]]
-            [clojure.string :as str]))
+  (:require
+    [utils.text :refer [text->lines]]
+    [year2023.day10.day10-data :refer [day10-example-1 day10-example-2
+                                       day10-puzzle]]
+    [ysera.test :refer [is=]]
+    [clojure.set :as set]))
+
+(def char->pipe
+  {\| #{:n :s}
+   \- #{:w :e}
+   \L #{:n :e}
+   \J #{:n :w}
+   \7 #{:s :w}
+   \F #{:s :e}
+   \. nil
+   \S :start})
+
+(defn text->grid
+  [text]
+  (->> text
+       (text->lines)
+       (map-indexed (fn [y line]
+                      (->> line
+                           (map-indexed (fn [x ch] {[x y] (char->pipe ch)})))))
+       (apply concat)
+       (apply merge)))
+
+(defn get-start 
+  [grid]
+  (->> grid
+       (filter (fn [[ _ pipe ]] (= pipe :start)))
+       (first)
+       (first)))
+
+(defn get-next-steps
+  [[sx sy :as start-location] grid]
+  (let [start-data (get grid start-location)]
+    (->> [[[(inc sx) sy] :e :w]
+          [[(dec sx) sy] :w :e]
+          [[sx (inc sy)] :s :n]
+          [[sx (dec sy)] :n :s]]
+         (filter (fn [[next-location local-match target-match]]
+                   (let [next-location-data (get grid next-location)]
+                   (and (or (= start-data :start)
+                            (contains? start-data local-match))
+                        (or (= next-location-data :start)
+                            (contains? next-location-data target-match))))))
+         (map (fn [[location & _]] location))
+         (into #{}))))
+
+(defn solve-distances 
+  [start grid]
+  (loop [step 0
+         distances {start 0}
+         next-steps #{start}]
+    (if (empty? next-steps)
+      distances
+      (let [next-steps (->> next-steps
+                            (map (fn[step] (get-next-steps step grid)))
+                            (apply set/union))
+            next-steps (set/difference next-steps (->> distances (keys) (into #{})))
+            step (inc step) 
+            distances (reduce (fn [distances location]
+                                (assoc distances location step))
+                              distances
+                              next-steps)]
+        (recur step
+               distances
+               next-steps)))))
 
 (defn day10-part1
   {:test (fn []
-           (is= (day10-part1 day10-example) 0))}
+           (is= (day10-part1 day10-example-1) 4)
+           (is= (day10-part1 day10-example-2) 8))}
   [text]
-  )
-
+  (let [grid (text->grid text)
+        start (get-start grid)]
+    (->> (solve-distances start grid)
+         (vals)
+         (reduce max))))
 
 (comment
   (time (day10-part1 day10-puzzle))
-  ;
+  ; (out) "Elapsed time: 4166.956671 msecs"
+  ; 6882
   )
--- src/year2023/day10/day10_part2.clj
+++ src/year2023/day10/day10_part2.clj
@@ -1,17 +1,85 @@
 (ns year2023.day10.day10-part2
-  (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2023.day10.day10-part1 :as part-1]
-            [year2023.day10.day10-data :refer [day10-example day10-puzzle]]
-            [clojure.string :as str]))
+  (:require
+    [year2023.day10.day10-part1 :as part-1]
+    [year2023.day10.day10-data :refer [day10-example-3 day10-example-4
+                                       day10-example-5 day10-puzzle]]
+    [ysera.test :refer [is=]]))
+
+(defn get-start-pipes
+  [[sx sy] grid]
+  (->> [[[(inc sx) sy] :e :w]
+        [[(dec sx) sy] :w :e]
+        [[sx (inc sy)] :s :n]
+        [[sx (dec sy)] :n :s]]
+       (filter (fn [[next-location _ target-match]]
+                 (let [next-location-data (get grid next-location)]
+                   (contains? next-location-data target-match))))
+       (map (fn [[_ local-match _]] local-match))
+       (into #{})))
+
+(defn remap-line 
+  [y the-loop grid width]
+  (->> 
+    (for [x (range 0 width)]
+      (let [location [x y]]
+        (if (contains? the-loop location)
+          (get grid location)
+          :dot)))
+    (remove (fn [item] (= item #{:e :w})))))
+
+(defn- solve-line [y the-loop grid width]
+  (loop [[item & line] (remap-line y the-loop grid width)
+         prev-item nil
+         pipes 0
+         enclosed 0]
+    (cond
+      (nil? item)
+      enclosed
+
+      (= item #{:n :s})
+      (recur line nil (inc pipes) enclosed)
+
+      (and (= item :dot)
+           (odd? pipes))
+      (recur line nil pipes (inc enclosed))
+
+      (= item :dot)
+      (recur line nil pipes enclosed)
+
+      (nil? prev-item)
+      (recur line item pipes enclosed)
+     
+      (and (= prev-item #{:n :e}) (= item #{:s :w}))
+      (recur line nil (inc pipes) enclosed)
+
+      (and (= prev-item #{:s :e}) (= item #{:n :w}))
+      (recur line nil (inc pipes) enclosed)
+
+      :else
+      (recur line nil pipes enclosed))))
 
 (defn day10-part2
   {:test (fn []
-           (is= (day10-part2 day10-example) 0))}
+           (is= (day10-part2 day10-example-3) 4)
+           (is= (day10-part2 day10-example-4) 8)
+           (is= (day10-part2 day10-example-5) 10))}
   [text]
-  )
-
+  (let [grid (part-1/text->grid text)
+        start (part-1/get-start grid)
+        start-pipes (get-start-pipes start grid)
+        grid (assoc grid start start-pipes)
+        the-loop (->> (part-1/solve-distances start grid)
+                      (keys)
+                      (into #{}))
+        width (->> grid (keys) (map first) (reduce max) (inc))
+        height (->> grid (keys) (map second) (reduce max) (inc))]
+    (->> (range 0 height)
+         (map (fn [y] (solve-line y the-loop grid width)))
+         (concat)
+         (apply +))))
 
 (comment
-  (time (day10-part2 day10-puzzle))
-  ;
+  (time (day10-part2 day10-puzzle)) 
+  ; (out) "Elapsed time: 4488.518013 msecs"
+  ; 491
   )
