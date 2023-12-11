1b1c7c7f20656ef95b8667d9158307a025f63f30
Kenneth Hedman <github@vitberget.se>
2023-12-11 19:03:21 +0100

    year2023 day11

--- src/year2023/day11/day11_part1.clj
+++ src/year2023/day11/day11_part1.clj
@@ -1,16 +1,76 @@
 (ns year2023.day11.day11-part1
-  (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2023.day11.day11-data :refer [day11-example day11-puzzle]]
-            [clojure.string :as str]))
+  (:require
+   [utils.numbers :refer [manhattan-distance]]
+   [utils.text :refer [text->lines]]
+   [year2023.day11.day11-data :refer [day11-example day11-example-grown
+                                      day11-puzzle]]
+   [ysera.test :refer [is=]]))
+
+(defn line->galaxies 
+  [y line]
+  (->> line
+       (map-indexed (fn [x ch] (when (= ch \#) [x y])))
+       (filter identity)))
+
+(defn text->galaxies
+  [text]
+  (->> text
+       (text->lines)
+       (map-indexed line->galaxies)
+       (apply concat)))
+
+(defn find-empty-columns 
+  [{galaxies :galaxies width :width}]
+  (->> (range 0 width)
+       (filter (fn [col-x] (->> galaxies
+                                (filter (fn [[x _]] (= col-x x)))
+                                (empty?))))))
+
+(defn find-empty-rows 
+  [{galaxies :galaxies height :height}]
+  (->> (range 0 height)
+       (filter (fn [col-y] (->> galaxies
+                                (filter (fn [[_ y]] (= col-y y)))
+                                (empty?))))))
+
+(defn grow-galaxy 
+  {:test (fn[] 
+           (let [galaxies (text->galaxies day11-example)
+                 grown-galaxies (text->galaxies day11-example-grown)
+                 width (->> galaxies (map first) (reduce max) (inc))
+                 height (->> galaxies (map second) (reduce max) (inc))
+                 empty-columns (find-empty-columns {:galaxies galaxies :width width})
+                 empty-rows (find-empty-rows {:galaxies galaxies :height height}) ]
+             (is= (grow-galaxy {:galaxies galaxies :empty-columns empty-columns :empty-rows empty-rows})
+                  grown-galaxies)))}
+  [{galaxies :galaxies empty-columns :empty-columns empty-rows :empty-rows}]
+  (->> galaxies
+       (map (fn [[x y]] [(->> empty-columns (filter (fn [c] (< c x))) (count) (+ x)) 
+                         (->> empty-rows (filter (fn [r] (< r y))) (count) (+ y))]))))
+
+(defn calculate-galaxies-distances [galaxy galaxies]
+  (->> galaxies
+       (map (fn [galaxy-b] (manhattan-distance galaxy galaxy-b)))
+       (apply +)))
 
 (defn day11-part1
-  {:test (fn []
-           (is= (day11-part1 day11-example) 0))}
+  {:test (fn [] (is= (day11-part1 day11-example) 374))}
   [text]
-  )
-
+  (let [galaxies (text->galaxies text)
+        width (->> galaxies (map first) (reduce max) (inc))
+        height (->> galaxies (map second) (reduce max) (inc))
+        empty-columns (find-empty-columns {:galaxies galaxies :width width})
+        empty-rows (find-empty-rows {:galaxies galaxies :height height})
+        grown-galaxies (grow-galaxy {:galaxies galaxies :empty-columns empty-columns :empty-rows empty-rows})]
+      (loop [[galaxy & galaxies] grown-galaxies
+             summary 0]
+        (if (empty? galaxies)
+          summary
+          (recur galaxies
+                 (+ summary (calculate-galaxies-distances galaxy galaxies)))))))
 
 (comment
   (time (day11-part1 day11-puzzle))
-  ;
+  ; (out) "Elapsed time: 122.041186 msecs"
+  ; 9543156
   )
--- src/year2023/day11/day11_part2.clj
+++ src/year2023/day11/day11_part2.clj
@@ -1,17 +1,44 @@
 (ns year2023.day11.day11-part2
-  (:require [ysera.test :refer [is is= is-not deftest]]
+  (:require [ysera.test :refer [is=]]
             [year2023.day11.day11-part1 :as part-1]
-            [year2023.day11.day11-data :refer [day11-example day11-puzzle]]
-            [clojure.string :as str]))
+            [year2023.day11.day11-data :refer [day11-example day11-puzzle]]))
 
+(defn grow-galaxy 
+  [{galaxies :galaxies empty-columns :empty-columns empty-rows :empty-rows factor :factor}]
+  (->> galaxies
+       (map (fn [[x y]] [(->> empty-columns 
+                              (filter (fn [c] (< c x))) 
+                              (count) 
+                              (* factor)
+                              (+ x)) 
+                         (->> empty-rows 
+                              (filter (fn [r] (< r y)))
+                              (count) 
+                              (* factor)
+                              (+ y))]))))
 (defn day11-part2
   {:test (fn []
-           (is= (day11-part2 day11-example) 0))}
-  [text]
-  )
-
+           (is= (day11-part2 day11-example 10) 1030)
+           (is= (day11-part2 day11-example 100) 8410))}
+  [text spaces]
+  (let [galaxies (part-1/text->galaxies text)
+        width (->> galaxies (map first) (reduce max) (inc))
+        height (->> galaxies (map second) (reduce max) (inc))
+        empty-columns (part-1/find-empty-columns {:galaxies galaxies :width width})
+        empty-rows (part-1/find-empty-rows {:galaxies galaxies :height height})
+        grown-galaxies (grow-galaxy {:galaxies galaxies 
+                                     :empty-columns empty-columns 
+                                     :empty-rows empty-rows
+                                     :factor (dec spaces)})]
+      (loop [[galaxy & galaxies] grown-galaxies
+             summary 0]
+        (if (empty? galaxies)
+          summary
+          (recur galaxies
+                 (+ summary (part-1/calculate-galaxies-distances galaxy galaxies)))))))
 
 (comment
-  (time (day11-part2 day11-puzzle))
-  ;
+  (time (day11-part2 day11-puzzle 1000000))
+  ; (out) "Elapsed time: 77.87378 msecs"
+  ; 625243292686
   )
