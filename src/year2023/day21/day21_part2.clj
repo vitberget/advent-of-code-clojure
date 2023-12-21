c8f327cbe626a819d9173ad8ae1f3eeef4d00b0f
Kenneth Hedman <github@vitberget.se>
2023-12-21 13:27:21 +0100

    year2023 day21 part2 - wip

--- src/year2023/day21/day21_part2.clj
+++ src/year2023/day21/day21_part2.clj
@@ -6,12 +6,19 @@
 
 (defn day21-part2
   {:test (fn []
-           (is= (day21-part2 day21-example) 0))}
-  [text]
-  )
+           (is= (day21-part2 day21-example 6) 16)
+           (is= (day21-part2 day21-example 10) 50)
+           (is= (day21-part2 day21-example 50) 1594)
+           (is= (day21-part2 day21-example 100) 6536)
+           (is= (day21-part2 day21-example 500) 167004)
+           (is= (day21-part2 day21-example 1000) 668697)
+           (is= (day21-part2 day21-example 5000) 16733044))}
+  [text steps]
+  (let [the-map (part-1/text->map text)]
+  ))
 
 
 (comment
-  (time (day21-part2 day21-puzzle))
+  (time (day21-part2 day21-puzzle 26501365))
   ;
   )
