66854a5eff973c0926a0a1bb30e63c666629aaa3
Kenneth Hedman <github@vitberget.se>
2024-12-23 14:36:04 +0100

    year2024 day23 part1

--- src/year2024/day23/day23_part1.clj
+++ src/year2024/day23/day23_part1.clj
@@ -1,16 +1,56 @@
 (ns year2024.day23.day23-part1
-  (:require [ysera.test :refer [is is= is-not deftest]]
-            [year2024.day23.day23-data :refer [day23-example day23-puzzle]]
-            [clojure.string :as str]))
+  (:require
+    [utils.text :as ut]
+    [year2024.day23.day23-data :refer [day23-example day23-puzzle]]
+    [ysera.test :refer [is=]]
+    [clojure.string :as str]
+    [clojure.set :as set]))
+
+(defn text->connections
+  [text]
+  (->> text
+       (ut/text->lines)
+       (map (fn[line]  (->> line
+                            (ut/split-on #"-"))))))
+
+(defn connections->directional
+  [connections]
+  (reduce (fn [directional [computer-1 computer-2]]
+            (-> directional
+                (update computer-1 (fn [set-of-computer] (conj (or set-of-computer #{}) computer-2)))
+                (update computer-2 (fn [set-of-computer] (conj (or set-of-computer #{}) computer-1)))))
+          {}
+          connections))
+
+(defn contains-links? 
+  [t computers directional]
+  (loop [[computer & computers] computers
+         threes (list)]
+    (if (nil? computer)
+      threes
+    (let [other-computers (into #{} computers)
+          how (set/intersection (get directional computer) other-computers)
+          more-threes (->> how
+                           (map (fn [other] #{t computer other})))]
+        (recur computers (concat threes more-threes))))))
 
 (defn day23-part1
-  {:test (fn []
-           (is= (day23-part1 day23-example) 0))}
+  {:test (fn [] (is= (day23-part1 day23-example) 7))}
   [text]
-  )
-
+  (let [directional (->> text
+                         (text->connections)
+                         (connections->directional))
+        t-s (->> directional
+                 (keys)
+                 (filter (fn[computer] (str/starts-with? computer "t"))))]
+    (->> t-s
+         (map (fn [t] [t (get directional t)]))
+         (mapcat (fn [[t computers]] (contains-links? t computers directional)))
+         (into #{})
+         (count))))
 
 (comment
   (time (day23-part1 day23-puzzle))
-  ;
+  ; "Elapsed time: 16.6698 msecs"
+  ; 1423
   )
